75aba2ac306ea54fb7a803fde69bd2562aa514ff
David <davidllamas@hotmail.com>
2026-08-07 15:54:59 +0700

    Initial Commit

---
+++ jarvis-project/jarvis_projectApp.swift
@@ -0,0 +1,17 @@
+//
+//  jarvis_projectApp.swift
+//  jarvis-project
+//
+//  Created by David Llamas on 07/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct jarvis_projectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
