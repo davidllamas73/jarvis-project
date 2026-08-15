b504be7237f6f626c938895afb8bbe54f70ddb5c
David <davidllamas@hotmail.com>
2026-08-15 20:29:14 +0700

    Add JarvisiOS target scaffold

---
+++ JarvisiOS/JarvisiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  JarvisiOSApp.swift
+//  JarvisiOS
+//
+//  Created by David Llamas on 15/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct JarvisiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
